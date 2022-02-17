0042ddc3403fd6af1b6a82017e64d65370d007f8
enalenaral <yulya43va@yandex.ru>
2022-02-17 20:17:07 +0300

    Second commit

--- Data.py
+++ Data.py
@@ -1,29 +1,28 @@
+import telebot
 import sqlite3
+
+bot = telebot.TeleBot('5209932574:AAFJpNe9bt1fhMma-jwGNP5YSoGJqWjcrh8')
 
 @bot.message_handler(commands=['start'])
 def start(message):
     connect = sqlite3.connect('users.db')
     cursor = connect.cursor()
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS login_id(
-   id  INTEGER
+    cursor.execute("""CREATE TABLE IF NOT EXISTS nickname(
+        id INTEGER
     )""")
-
     connect.commit()
 
-    user_id = [message.chat.id]
-    cursor.execute("INSERT INTO login_id VALUES(?);", user_id)
-    connect.commit()
 
     people_id = message.chat.id
-    cursor.execute("SELECT id FROM login_id WHERE id = {people_id}")
+    cursor.execute(f"SELECT id FROM users.db WHERE id = {people_id}")
     data = cursor.fetchone()
     if data is None:
-        user_id = [message.chat.id]
-        cursor.execute("INSERT INTO login _id VALUES(?);", user_id)
+        students_list = [message.chat.id]
+        cursor.execute("INSERT INTO nickname VALUES(?);", students_list)
         connect.commit()
     else:
-        bot.send_message(message.chat.id, '...')
+        bot.send_message(message.chat.id, 'Такой уже существует')
 
 
 @bot.message_handler(commands=['delete'])
@@ -31,3 +30,10 @@ def delete(message):
     connect = sqlite3.connect('users.db')
     cursor = connect.cursor()
 
+    people_id = message.chat.id
+    cursor.execute(f"DELETE FROM nickname WHERE id = {people_id}")
+    connect.commit()
+
+
+
+bot.polling()
